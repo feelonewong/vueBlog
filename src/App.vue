2153fd7da40140ffb9ac674a14c7f016c991249a
feelonewong <feelonewong@gmail.com>
2022-12-01 23:19:38 +0800

    update:04博客首页搭建

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,7 @@
 <script setup>
-import { RouterView } from 'vue-router'
-import { getCurrentInstance } from 'vue';
-import { StarOutlined, StepBackwardOutlined } from '@ant-design/icons-vue';
-const {proxy}  = getCurrentInstance();
-
-const info = ()=>{
-  console.log("info")
-  proxy.$message.info("Hello world!");
-}
-
-
 </script>
 
 <template>
-    <a-button type="primary" @click="info">Primary Button</a-button>
-    <a-button>Default Button</a-button>
-    <a-button type="dashed">Dashed Button</a-button>
-    <a-button type="text">Text Button</a-button>
-    <a-button type="link">Link Button</a-button>
-    <star-outlined />
-    <step-backward-outlined />
   <RouterView />
 </template>
 
