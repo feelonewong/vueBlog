c301a120a436a6ae70f16d5a651a5f42b77d641f
feelonewong <feelonewong@gmail.com>
2022-12-04 11:55:23 +0800

    update:06父子组件传值articleItem

--- src/components/ArticleItem.vue
+++ src/components/ArticleItem.vue
@@ -1,19 +1,63 @@
 <template>
-  <div class="box">
-    <h1>
-      <router-link to="/">文章标题</router-link>
-    </h1>
+  <a-typography class="box">
+    <a-typography-title :level="3">
+      <router-link :to="props.link">{{props.title}}</router-link>
+    </a-typography-title>
     <a-space>
-      <span>作者: 张三</span>
-      <span>分类：Vue3</span>
-      <span>发布日期: 2002年11月6日</span>
+      
+        <a-space>
+          <user-outlined></user-outlined>
+          <span>作者：<slot name="author">{{props.author}}</slot>  </span>
+        </a-space>
+      
+        <a-space>
+          <folder-outlined></folder-outlined>
+          <span>分类：{{props.category}} <slot name="category">{{props.category}}</slot></span>
+        </a-space>
+      
+      
+        <a-space>
+          <clock-circle-outlined></clock-circle-outlined>
+          <span>发布时间：{{props.pubdate}} <slot name="pubdate">{{props.pubdate}}</slot></span>
+        </a-space>
+      
     </a-space>
-  </div> 
+    <a-typography-paragraph type="secondary">
+      {{props.desc}}
+    </a-typography-paragraph>
+  </a-typography>
 </template>
 
 
 <script setup>
-
+import {FolderOutlined, UserOutlined, ClockCircleOutlined } from "@ant-design/icons-vue"
+import { RouterLink } from "vue-router";
+const props = defineProps({
+  title: {
+    type: String,
+    default: "默认文章标题"
+  },
+  link: {
+    type: String || Object,
+    default: "/"
+  },
+  author: {
+    type: String,
+    default: "张三"
+  },
+  category: {
+    type: String,
+    default: "React"
+  },
+  pubdate: {
+    type: String,
+    default: "2022年12月3日"
+  },
+  desc: {
+    type: String,
+    default: "本课程作为前端开发组件化的入门实战课程，力求精简，将通过一个博客的案例整合相关的技术点，以及每一个技术点在实战中的基础应用，以实战为最终目的，力求能带领大家快速上手"
+  }
+})
 </script>
 
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,37 @@
 <template>
-      <article-item v-for="item in 5" :key="item"></article-item>
+      <article-item v-for="(item, index) in articleDatas" :key="index" 
+            :title="item.title"
+            :link="item.link"
+      >
+            <template #author>{{item.author}}</template>
+            <template #category>{{item.category}}</template>
+            <template #pubdate>{{item.pubdate}}</template>
+      </article-item>
+      
 </template>
 
 
 <script setup>
+import { reactive } from "@vue/reactivity";
 
+const articleDatas = reactive([
+ { 
+   title :"JavaScript入门③-函数(2)原理{深入}执行上下文 ",
+   link: "/",
+   desc: "摘要：  JavaScript入门③-函数(2)原理{深入}执行上下文被JavaScript的闭包、上下文、嵌套函数、this搞得很头痛，这语言设计的，感觉比较混乱，先勉强理解总结一下😂😂😂。● 为什么有闭包这么个东西？闭包包的是什么？● 什么是词法作用域？● 函数是如执行的呢？",
+   author: "王小虎",
+   category: "JavaScript",
+   pubdate: "2022年12月3日"
+  },
+  { 
+   title :"CSS宝典⑦-CSS处理器 Less/Scss",
+   link: "/",
+   desc: "摘要：  JavaScript入门③-函数(2)原理{深入}执行上下文被JavaScript的闭包、上下文、嵌套函数、this搞得很头痛，这语言设计的，感觉比较混乱，先勉强理解总结一下😂😂😂。● 为什么有闭包这么个东西？闭包包的是什么？● 什么是词法作用域？● 函数是如执行的呢？",
+   author: "韩国为",
+   category: "CSS",
+   pubdate: "2013年12月3日"
+  }
+])
 </script>
 
 
